5073c0f382f5077b54ac1f75b7f2780ff50231fb
asmsuechan <suenagaryoutaabc@gmail.com>
2018-03-20 17:25:23 +0900

    Add main.go and travisci environment for releasing to GitHub Releases

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/platforms/mqtt"
+	"os"
+	"reflect"
+
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/raspi"
+)
+
+func main() {
+	// mqtt.DEBUG = log.New(os.Stdout, "", 0)
+	// mqtt.ERROR = log.New(os.Stdout, "", 0)
+	uri := os.Getenv("GENKAN_URI")
+	id := "device"
+	userName := os.Getenv("GENKAN_USERNAME")
+	password := os.Getenv("GENKAN_PASSWORD")
+	mqttAdaptor := mqtt.NewAdaptorWithAuth(uri, id, userName, password)
+
+	adaptor := raspi.NewAdaptor()
+	servo := gpio.NewServoDriver(adaptor, "12")
+
+	work := func() {
+		servo.Move(uint8(27))
+
+		mqttAdaptor.On("test", func(msg mqtt.Message) {
+			if reflect.DeepEqual(msg.Payload(), []byte("open")) {
+				servo.Move(uint8(31))
+			} else if reflect.DeepEqual(msg.Payload(), []byte("close")) {
+				servo.Move(uint8(13))
+			}
+		})
+	}
+
+	robot := gobot.NewRobot("mqttBot",
+		[]gobot.Connection{mqttAdaptor},
+		work,
+	)
+
+	robot.Start()
+}
