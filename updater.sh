a3b3fec44df264bb3a9e9781e834888417503e2c
asmsuechan <suenagaryoutaabc@gmail.com>
2018-03-24 09:09:46 +0900

    Change to mv binary to $HOME

--- updater.sh
+++ updater.sh
@@ -3,9 +3,11 @@
 cd /home/pi
 curl -sJLO https://github.com/asmsuechan/genkan_device/releases/download/v_$1/genkan_device_$1_linux_arm.tar.gz
 tar -zxvf genkan_device_$1_linux_arm.tar.gz
+mv genkan_device_$1_linux_arm/genkan_device $HOME
 
 # Stop existing process
 kill `ps -ef | grep genkan_device | awk '{print $2;}'` 2>/dev/null
 
 # Run the downloaded program and pi-blaster
-./genkan_device_$1_linux_arm/genkan_device
+$HOME/genkan_device
+rm -rf genkan_device_$1_linux_arm/
