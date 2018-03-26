8b49074c98efc747b8895fc5f0d1aaefa0abd434
asmsuechan <suenagaryoutaabc@gmail.com>
2018-03-26 09:46:30 +0900

    Fix updater.sh accompanied by the changes of systemctl and bump v0.0.18

--- updater.sh
+++ updater.sh
@@ -3,11 +3,8 @@
 cd /home/pi
 curl -sJLO https://github.com/asmsuechan/genkan_device/releases/download/v_$1/genkan_device_$1_linux_arm.tar.gz
 tar -zxvf genkan_device_$1_linux_arm.tar.gz
-mv genkan_device_$1_linux_arm/genkan_device $HOME
-
-# Stop existing process
-kill `ps -ef | grep genkan_device | awk '{print $2;}'` 2>/dev/null
+mv genkan_device_$1_linux_arm/genkan_device /home/pi
 
 # Run the downloaded program and pi-blaster
 sudo systemctrl restart genkan
-rm -rf genkan_device_$1_linux_arm/
+rm -rf genkan_device_$1_linux_arm/ genkan_device_$1_linux_arm.tar.gz
